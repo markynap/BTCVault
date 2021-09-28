2ca7c23ebb9c3c9ce15376475b96ea4ab4d7988a
Mark Nap <55161831+markynap@users.noreply.github.com>
2021-09-28 15:54:31 -0400

    Update IDistributor.sol

--- IDistributor.sol
+++ IDistributor.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.4;
 
 interface IDistributor {
-    function setDistributionCriteria(uint256 _minPeriod, uint256 _minDistribution) external;
+    function setDistributionCriteria(uint256 _minPeriod, uint256 _minMainDistribution, uint256 _minParentDistribution) external;
     function setShare(address shareholder, uint256 amount) external;
     function deposit() external;
     function process(uint256 gas) external;
